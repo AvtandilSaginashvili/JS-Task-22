de60e6a2d2fe84a67eb03d676b7010f26780392c
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-06-22 19:39:55 +0400

    update

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var x = 1;
 var y = setInterval (function(){ 
     document.getElementById("num").innerHTML = Number(x);
     x++;
-    clearInterval(y);
+    if (x == 21) {
+        clearInterval(y);
+    }
 }, 200);
     
